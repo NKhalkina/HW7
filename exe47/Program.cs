fa43f6448d422612b602ad5fb803eefdd8433c1d
Natasha Khalkina <tashik2010@mail.ru>
2022-12-19 14:05:47 +0300

    Решение задачи 47

---
+++ exe47/Program.cs
@@ -0,0 +1,58 @@
+﻿/*Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9*/
+//получить числа с консоли
+int GetNumber(string message)
+{
+    int result = 0;
+    while(true)
+    {
+        Console.WriteLine(message);
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+    return result;
+}
+
+//задать матрицу и заполнить ее числами
+double[,] GetMatrix(int m, int n)
+{
+    double[,] matrix = new double[m,n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Convert.ToDouble(rnd.Next(-100, 100)/10.0);
+        }
+    }
+    return matrix;
+}
+
+//распечатать матрицу
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }  
+}
+
+int m = GetNumber("Введите количество строк: ");
+int n = GetNumber("Введите количество столбцов: ");
+
+double[,] matrix = GetMatrix(m,n);
+PrintMatrix(matrix);
