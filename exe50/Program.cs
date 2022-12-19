c5992409eb0ce9b37942f13986dd28e7c2211baa
Natasha Khalkina <tashik2010@mail.ru>
2022-12-19 14:09:23 +0300

    Решение задачи 50

---
+++ exe50/Program.cs
@@ -0,0 +1,73 @@
+﻿/*Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет*/
+
+int GetNumber(string message)
+{
+    int result = 0;
+    while(true)
+    {
+        Console.WriteLine(message);
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+    return result;
+}
+
+//задать матрицу и заполнить ее числами
+int[,] GetMatrix(int m, int n)
+{
+    int[,] matrix = new int[m,n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(0, 11);
+        }
+    }
+    return matrix;
+}
+
+//распечатать матрицу
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }  
+}
+
+
+int i = GetNumber("Введите m ");
+int j = GetNumber("Введите n");
+
+int[,] matrix = GetMatrix(i,j);
+PrintMatrix(matrix);
+
+
+int a = GetNumber("введите строку позиции элемента ");
+int b = GetNumber("введите столбец позиции элемента ");
+int matr = matrix[a,b];
+
+if (i > a && j > b)
+{
+    Console.WriteLine($"{matr} ");
+}
+else
+    Console.WriteLine("такого числа в массиве нет");
